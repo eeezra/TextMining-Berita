8c583284302269677ec1c4155c8aca1fd7f30238
sariyyan izzati <sariyyan.ikp@gmail.com>
2026-06-21 15:25:10 +0700

    Update summarizer.py

--- utils/summarizer.py
+++ utils/summarizer.py
@@ -1,51 +1,22 @@
-import re
-import numpy as np
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
-from sklearn.feature_extraction.text import (
-TfidfVectorizer
-)
+def summarize_text(text, num_sentences=3):
 
-def summarize_text(
-    text,
-    num_sentences=3
-    ):
-    
-    sentences = re.split(
-        r'(?<=[.!?])\\s+',
-        text
+    parser = PlaintextParser.from_string(
+        text,
+        Tokenizer("indonesian")
     )
-    
-    sentences = [
-        s.strip()
-        for s in sentences
-        if len(s.strip()) > 20
+
+    summarizer = LsaSummarizer()
+
+    summary = summarizer(
+        parser.document,
+        num_sentences
+    )
+
+    return [
+        str(sentence)
+        for sentence in summary
     ]
-    
-    if len(sentences) <= num_sentences:
-    
-        return sentences
-    
-    vectorizer = TfidfVectorizer()
-    
-    matrix = vectorizer.fit_transform(
-        sentences
-    )
-    
-    scores = np.asarray(
-        matrix.sum(axis=1)
-    ).flatten()
-    
-    top_idx = np.argsort(
-        scores
-    )[-num_sentences:]
-    
-    top_idx = sorted(
-        top_idx
-    )
-    
-    summary = [
-        sentences[i]
-        for i in top_idx
-    ]
-    
-    return summary
