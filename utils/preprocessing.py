33cafcb54c3040589e55334142c035891f51488f
sariyyan izzati <sariyyan.ikp@gmail.com>
2026-06-21 15:19:12 +0700

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -34,47 +34,47 @@ custom_stopwords
 
 def preprocess_text(text):
 
-text = str(text)
-
-# lowercase
-text = text.lower()
-
-# remove url
-text = re.sub(
-    r"http\\S+|www\\S+",
-    "",
-    text
-)
-
-# remove punctuation
-text = re.sub(
-    r"[^\\w\\s]",
-    " ",
-    text
-)
-
-# remove numbers
-text = re.sub(
-    r"\\d+",
-    " ",
-    text
-)
-
-# remove extra spaces
-text = re.sub(
-    r"\\s+",
-    " ",
-    text
-).strip()
-
-# tokenization
-words = text.split()
-
-# stopword removal
-words = [
-    word
-    for word in words
-    if word not in stopwords
-]
-
-return " ".join(words)
+    text = str(text)
+    
+    # lowercase
+    text = text.lower()
+    
+    # remove url
+    text = re.sub(
+        r"http\\S+|www\\S+",
+        "",
+        text
+    )
+    
+    # remove punctuation
+    text = re.sub(
+        r"[^\\w\\s]",
+        " ",
+        text
+    )
+    
+    # remove numbers
+    text = re.sub(
+        r"\\d+",
+        " ",
+        text
+    )
+    
+    # remove extra spaces
+    text = re.sub(
+        r"\\s+",
+        " ",
+        text
+    ).strip()
+    
+    # tokenization
+    words = text.split()
+    
+    # stopword removal
+    words = [
+        word
+        for word in words
+        if word not in stopwords
+    ]
+    
+    return " ".join(words)
